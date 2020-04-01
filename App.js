20a1ff3dc089896b356c70d8721d7e07a57ecb92
2segundo <josevitorsegundo@hotmail.com>
2020-04-01 16:12:46 -0300

    Criação do Routes para rotas

--- App.js
+++ App.js
@@ -1,42 +1,10 @@
 import React, { Component } from 'react';
-import { createAppContainer, NavigationActions } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import { YellowBox, View, TouchableOpacity,Button, Dimensions } from 'react-native';
-import { createDrawerNavigator, DrawerActions } from 'react-navigation-drawer';
-import Icon from 'react-native-vector-icons/FontAwesome';
-YellowBox.ignoreWarnings(['Accessing view manager'])
-
-//alterar textos
-//Import das screens
-import ChamaDiario from './src/screens/ChamaDiario';
-import CuidadosPage from './src/screens/CuidadosPage';
-import ChamaTextGeral from './src/screens/ChamaTextGeral';
-import EstiloVida from './src/screens/ChamaEstiloVida';
-import ChamaCalendario from './src/screens/ChamaCalendario';
-
-//var {width} = Dimensions.get('window');
-
-const RootStackG = createStackNavigator({
-  Cuidados: CuidadosPage,
-  TextMostraGeral: ChamaTextGeral,
-  Estilo: EstiloVida,
-  Diário: ChamaCalendario,
-  Notas: ChamaDiario,
-},
-{
-  defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: '#9268D0',//'#B665A0',//
-     
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-      //width: Dimensions.get('window'),
-    },
-  },
+import Routes from "./src/route/Routes";
+export default class App extends Component {
+  render() {
+    return (
+      <Routes/>
+    );
+  }
 }
-);
 
-const App = createAppContainer(RootStackG);
-export default App;
---
+++ src/route/Routes.js
@@ -0,0 +1,39 @@
+import { createAppContainer, NavigationActions } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { YellowBox } from 'react-native';
+//import Icon from 'react-native-vector-icons/FontAwesome';
+YellowBox.ignoreWarnings(['Accessing view manager']);
+
+//Import das screens
+import ChamaDiario from '../screens/ChamaDiario';
+import CuidadosPage from '../screens/CuidadosPage';
+import ChamaTextGeral from '../screens/ChamaTextGeral';
+import EstiloVida from '../screens/ChamaEstiloVida';
+import ChamaCalendario from '../screens/ChamaCalendario';
+
+//var {width} = Dimensions.get('window');
+
+const RootStackG = createStackNavigator({
+  Cuidados: CuidadosPage,
+  TextMostraGeral: ChamaTextGeral,
+  Estilo: EstiloVida,
+  Diário: ChamaCalendario,
+  Notas: ChamaDiario,
+},
+{
+  defaultNavigationOptions: {
+    headerStyle: {
+      backgroundColor: '#9268D0',//'#B665A0',//
+     
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+      //width: Dimensions.get('window'),
+    },
+  },
+}
+);
+
+const Routes = createAppContainer(RootStackG);
+export default Routes;
