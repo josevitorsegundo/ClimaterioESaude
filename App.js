3951c2c4cf2dd5481c018de302cbe23aa596d699
2segundo <josevitorsegundo@hotmail.com>
2020-07-26 20:29:29 -0300

    ignorar aviso

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Routes from "./src/route/Routes";
 import { YellowBox } from 'react-native';
 
 YellowBox.ignoreWarnings(['Accessing view manager']);
+YellowBox.ignoreWarnings(['Calling']);
 
 export default class App extends Component {
   render() {
